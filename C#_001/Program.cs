155c529468056e0a01abe3e2c67b45c79cdb05bb
namubar <mubarakshin.nail@tatar.ru>
2022-10-19 16:02:52 +0300

    Первая попытка

--- C#_001/Program.cs
+++ C#_001/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число: ");
+int a=int.Parse (Console.ReadLine ()!);
+Console.WriteLine("Введите второе число: ");
+int b= int.Parse (Console.ReadLine ()!);
+int max;
+if (a!=b)
+    {if (a>b) 
+        {max =a;
+        Console.WriteLine ("Max = "+ max);
+        }
+    else
+        {max=b;
+        Console.WriteLine ("Max = "+ max);
+        }
+}         
+else 
+{Console.WriteLine ("Числа равны");
+} 
